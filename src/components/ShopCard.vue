6278fb019a6a333cecee19824f30bc62a914411a
Erik Küchler <erik.kuechler@student.htw-berlin.de>
2023-12-13 16:02:36 +0100

    Shop grid added

---
+++ src/components/ShopCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Baum_Christmas from '../assets/Baum_Christmas.png'
+</script>
+
+<template>
+
+    <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+        <img :src="Baum_Christmas" class="w-64 p-8 rounded-t-lg mx-auto" alt="Baum_Christmas">
+        <div class="px-5 pb-5">
+            <h5 class="text-xl font-semibold tracking-tight text-gray-900">Weihnachtsbaum</h5>
+            <div class="flex items-center justify-between">
+                <span class="text-3xl font-bold text-gray-900">60 Coins</span>
+                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Kaufen</a>
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,11 +1,20 @@
 <script setup>
-import CoinTest from '../components/CoinTest.vue';
+import ShopCard from '../components/ShopCard.vue';
 </script>
 
 <template>
   <div class="main bg-gray-100">
-    <h1 class="text-7xl font-bold">Shop</h1>
-    <CoinTest/>
+    <div class="max-w-screen-xl p-4 mt-4 mx-auto">
+      <h1 class="text-6xl font-bold">Shop</h1>
+      <div id="productgrid" class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-y-14 gap-x-14 mt-10 mb-5 justify-items-center justify-center">
+        <ShopCard/>
+        <ShopCard/>
+        <ShopCard/>
+        <ShopCard/>
+        <ShopCard/>
+        <ShopCard/>
+      </div>
+    </div>
   </div>
 </template>
 
