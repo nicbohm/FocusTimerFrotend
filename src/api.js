d2c635688a9e9aba6080229113d50400b290bdfb
Erik Küchler <erik.kuechler@student.htw-berlin.de>
2024-01-11 17:51:08 +0100

    deployment with render

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+// const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://focus-timer-backend.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
