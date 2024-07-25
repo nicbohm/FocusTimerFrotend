ab512e64b37f29067da45721487c08385725f64a
Erik Küchler <erik.kuechler@student.htw-berlin.de>
2024-07-25 11:58:09 +0200

    vite env

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const BASE_URL = 'http://localhost:8080';
 // const BASE_URL = 'https://focus-timer-backend.onrender.com';
-const BASE_URL = process.env.API_URL || 'http://localhost:8080';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
