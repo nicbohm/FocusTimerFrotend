4a34efc724fe1adac6a13a11fb10c4d688ed81c3
Erik Küchler <erik.kuechler@student.htw-berlin.de>
2024-07-25 11:35:25 +0200

    add api env

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const BASE_URL = 'http://localhost:8080';
-const BASE_URL = 'https://focus-timer-backend.onrender.com';
+// const BASE_URL = 'https://focus-timer-backend.onrender.com';
+const BASE_URL = '${API_URL}';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
